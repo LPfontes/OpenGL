b4234b6e955eac2da622ed718f5459bff9e7d0b2
Luiz paulo de souza fontes junior <82241012+LPfontes@users.noreply.github.com>
2026-02-11 21:04:51 -0300

    Add files via upload

--- robot_vis.c
+++ robot_vis.c
@@ -36,58 +36,48 @@
  */
 
 /*
- *  cube.c
- *  This program demonstrates a single modeling transformation,
- *  glScalef() and a single viewing transformation, gluLookAt().
- *  A wireframe cube is rendered.
+ * robot.c
+ * This program shows how to composite modeling transformations
+ * to draw translated and rotated hierarchical models.
+ * Interaction:  pressing the s and e keys (shoulder and elbow)
+ * alters the rotation of the robot arm.
  */
 #include <GL/glut.h>
 #include <stdlib.h>
 
-static float blue_x = 0.0;
-static int invert_order = 0;
+static int shoulder = 0, elbow = 0;
 
 void init(void) 
 {
    glClearColor (0.0, 0.0, 0.0, 0.0);
    glShadeModel (GL_FLAT);
-}
-
-void drawBox(GLfloat r, GLfloat g, GLfloat b)
-{
-   glColor3f(r, g, b);
-   glScalef (1.0, 2.0, 1.0);
-   glutWireCube (1.0);
-   glutSolidCube(1.0);
+   glEnable(GL_DEPTH_TEST);
 }
 
 void display(void)
 {
-   glClear (GL_COLOR_BUFFER_BIT);
-   glColor3f (1.0, 0.0, 0.0);
-   glLoadIdentity ();             /* clear the matrix */
-           /* viewing transformation  */
-   gluLookAt (5.0, 5.0, 5.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0);
-
-   if (!invert_order) {
-      glPushMatrix();
-      glTranslatef(blue_x, 0.0, -3.0);
-      drawBox(0.0, 0.0, 1.0);
-      glPopMatrix();
-   }
-   // Draw red cube
+   glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
    glPushMatrix();
-   drawBox(1.0, 0.0, 0.0);
+   gluLookAt (2.0, 2.0, 2.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0);
+   glRotatef ((GLfloat) shoulder, 0.0, 0.0, 1.0);
+   glTranslatef (1.0, 0.0, 0.0);
+   glPushMatrix();
+   glColor3f(1.0, 0.0, 0.0);
+   glScalef (2.0, 0.4, 1.0);
+   glutSolidCube (1.0);
    glPopMatrix();
 
-   if (invert_order) {
-      glPushMatrix();
-      glTranslatef(blue_x, 0.0, -3.0);
-      drawBox(0.0, 0.0, 1.0);
-      glPopMatrix();
-   }
+   glTranslatef (0.8, 0.0, 0.0);
+   glRotatef ((GLfloat) elbow, 0.0, 1.0, 0.0);
+   glTranslatef (1.0, 0.0, 0.0);
+   glPushMatrix();
+   glColor3f(0.0, 0.0, 1.0);
+   glScalef (2.0, 0.4, 1.0);
+   glutSolidCube (1.0);
+   glPopMatrix();
 
-   glFlush ();
+   glPopMatrix();
+   glutSwapBuffers();
 }
 
 void reshape (int w, int h)
@@ -95,27 +85,35 @@ void reshape (int w, int h)
    glViewport (0, 0, (GLsizei) w, (GLsizei) h); 
    glMatrixMode (GL_PROJECTION);
    glLoadIdentity ();
-   glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-   glMatrixMode (GL_MODELVIEW);
+   gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glTranslatef (0.0, 0.0, -5.0);
 }
 
-void keyboard(unsigned char key, int x, int y)
+void keyboard (unsigned char key, int x, int y)
 {
    switch (key) {
       case 's':
-         blue_x += 0.2;
+         shoulder = (shoulder + 5) % 360;
          glutPostRedisplay();
          break;
-      case 'a':
-         blue_x -= 0.2;
+      case 'S':
+         shoulder = (shoulder - 5) % 360;
          glutPostRedisplay();
          break;
-      case 'i':
-         invert_order = !invert_order;
+      case 'e':
+         elbow = (elbow + 5) % 360;
+         glutPostRedisplay();
+         break;
+      case 'E':
+         elbow = (elbow - 5) % 360;
          glutPostRedisplay();
          break;
       case 27:
          exit(0);
+         break;
+      default:
          break;
    }
 }
@@ -123,7 +121,8 @@ void keyboard(unsigned char key, int x, int y)
 int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
-   glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+   glutInitDisplayMode (GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+   
    glutInitWindowSize (500, 500); 
    glutInitWindowPosition (100, 100);
    glutCreateWindow (argv[0]);
